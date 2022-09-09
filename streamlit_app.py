59b1fa635073e369465ec1413ebfdb1f112d3676
Lim Zhi Pei <95734402+limzhipei@users.noreply.github.com>
2022-09-09 13:20:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(
-     page_title="Ex-stream-ly Cool App",
+     page_title="This is Lit!",
      page_icon="random",
      layout="centered",
      initial_sidebar_state="collapsed",
@@ -30,25 +30,23 @@ In the meantime, below is an example of what you can do with just a few lines of
 with st.sidebar:
      st.radio('Select one:', [1, 2])
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 10000, 10)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+total_points = st.slider("Number of points in spiral", 1, 10000, 10)
+num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    points_per_turn = total_points / num_turns
+Point = namedtuple('Point', 'x y')
+data = []
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+points_per_turn = total_points / num_turns
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+for curr_point_num in range(total_points):
+    curr_turn, i = divmod(curr_point_num, points_per_turn)
+    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    radius = curr_point_num / total_points
+    x = radius * math.cos(angle)
+    y = radius * math.sin(angle)
+    data.append(Point(x, y))
+
+st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-
-    st.snow()
